5e0848156e7db10f028c939ac30037fab4c2e503
Kevin Stradtman <webkit.development.systems@gmail.com>
2022-05-10 23:13:38 -0400

    Added some of the layout for the bmi calculator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
+
 
 void main() => runApp(BMICalculator());
 
@@ -16,21 +18,3 @@ class BMICalculator extends StatelessWidget {
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
-      ),
-    );
-  }
-}
